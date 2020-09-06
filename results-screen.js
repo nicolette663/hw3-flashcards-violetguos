6ff23d28d3f0c8fd61eade9299dfb71b0bbcdb27
Y. Violet Guo <violetguos@users.noreply.github.com>
2020-09-06 15:56:30 -0300

    Forgot to add event to back to menu

--- results-screen.js
+++ results-screen.js
@@ -12,51 +12,53 @@ class ResultsScreen {
 
     this._onImperfectScore = this._onImperfectScore.bind(this);
     this._onPerfectScore = this._onPerfectScore.bind(this);
-
   }
 
   show(numRight, numWrong) {
     this.numRight = numRight;
     this.numWrong = numWrong;
-    this.containerElement.classList.remove('inactive');
+    this.containerElement.classList.remove("inactive");
 
-    const score = Math.round(this.numRight / (this.numRight + this.numWrong) * 100);
+    const score = Math.round(
+      (this.numRight / (this.numRight + this.numWrong)) * 100
+    );
     const scoreSpan = this.containerElement.querySelector(".percent");
     scoreSpan.textContent = score.toString();
-    const rightSpan =  this.containerElement.querySelector(".correct");
+    const rightSpan = this.containerElement.querySelector(".correct");
     const wrongSpan = this.containerElement.querySelector(".incorrect");
     rightSpan.textContent = this.numRight.toString();
     wrongSpan.textContent = this.numWrong.toString();
 
-    if(score < 100 || this.numWrong != 0){
+    if (score < 100 || this.numWrong != 0) {
       const button = this.containerElement.querySelector(".continue");
       console.log("button", button);
       button.textContent = "Continue";
-      button.addEventListener("click", this._onImperfectScore ); 
-
-    }
-    else{
+      button.addEventListener("click", this._onImperfectScore);
+    } else {
       const button = this.containerElement.querySelector(".continue");
       button.textContent = "Start Over";
-      button.addEventListener("click", this._onPerfectScore ); 
-
+      button.addEventListener("click", this._onPerfectScore);
     }
 
+    // wire up back to-menu
+    const buttonMenu = this.containerElement.querySelector(".to-menu");
+    buttonMenu.addEventListener("click", (event) => {
+      this.hide();
+      app.menu.show();
+    });
   }
 
   hide() {
-    this.containerElement.classList.add('inactive');
-    
+    this.containerElement.classList.add("inactive");
   }
 
-  _onImperfectScore(events){
-    document.dispatchEvent(new CustomEvent('continue-wrong-cards'));
+  _onImperfectScore(events) {
+    document.dispatchEvent(new CustomEvent("continue-wrong-cards"));
     this.hide();
   }
 
-  _onPerfectScore(event){
-    document.dispatchEvent(new CustomEvent('restart'));
+  _onPerfectScore(event) {
+    document.dispatchEvent(new CustomEvent("restart"));
     this.hide();
   }
-
 }
